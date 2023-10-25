e164751421666618d8fcf06759a917a7ba8a1ba2
anoooj0 <130328624+anoooj0@users.noreply.github.com>
2023-10-25 22:27:11 +0000

    Committed

--- conditionals.js
+++ conditionals.js
@@ -3,17 +3,27 @@
 // recommends an ice cream flavor based on the current temperature in Fahrenheit.
 
 // 1. Create a variable named temperature any value between 32 and 68.
+let temperature = 42;
+
+
 
 
 // 2. Create a variable named recommendedFlavor. Do not assign a value.
-
+let recommendedFlavor;
 
 // 3. Implement the recommendation logic
 // - If the temperature is less than 50, recommend "Hot Chocolate Fudge".
+if (temperature < 50) {
+  recommendedFlavor = "Hot Chocolate Fudge"
+}
 // - If the temperature is between 50 and 68 (inclusive), recommend "Caramel Crunch".
 // - If the temperature is greater than 68, recommend "Lemon Sorbet".
-
-
+else if (temperature <= 68) {
+  recommendedFlavor = "Caramel Crunch"
+}
+else {
+  recommendedFlavor = "Lemon Sorbet"
+}
 // 4. Console log the recommended flavor.
-
+console.log(recommendedFlavor);
 
--- variables.js
+++ variables.js
@@ -1,15 +1,19 @@
 // 1. Create a variable called `color` and assign it a string value of a color you like.
+let color = "White";
 
 
 // 2. Create a variable called `animal` and assign it a string value of an animal you like.
-
+let animal = "Dog";
 
 // 3. Create a variable called `creature`. 
 // - The calue should be the concatenation of the variable `color` and `animal`.
 // - Include a space!
+let creature = color + " " + animal;
 
 
 
 
 // 4. Uncomment the `message` variable and log it to the console.
-// let message = `I was on a walk today and saw a ${creature}. It was odd.`
+let message = `I was on a walk today and saw a ${creature}. It was odd.`;
+
+console.log(message);
